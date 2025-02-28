79bfa87d9da4f18064075d5a98af2ed8ba465d4c
GregToe <gr3gto3@gmail.com>
2025-02-28 15:58:25 -0800

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+import webbrowser
+
+# URL of the website to open
+url = "https://youvebeentrolledlol.github.io"
+
+# Open the URL in the default browser
+webbrowser.open(url)
